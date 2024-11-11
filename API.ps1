76464a9c3fbf70ff7d1cd6bf13c7b062e71bc13c
Ben Spilker <46803512+benspilker@users.noreply.github.com>
2024-11-11 13:27:09 -0500

    Delete API.ps1

--- API.ps1
+++
@@ -1,9 +0,0 @@
-$uri = "https://my.zerotier.com/api/network"
-$headers = @{"Authorization" = "bearer mPXKrT8RgK7bQgQg4xDvjJLcNrVdOE0M"}
-$response = Invoke-RestMethod -Uri $uri -Headers $headers
-foreach ($network in $response) {
-    if ($network.config.name -eq 'steve') {
-        $network.config.id
-    }
-}
-
